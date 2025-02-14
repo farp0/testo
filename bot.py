ec4c5650c921d89523b8a0a28cffbcdcf8c665fc
farp0 <167543216+farp0@users.noreply.github.com>
2025-02-14 22:03:13 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-
+import time
 # Replace these with your actual values
 api_id = "3335796"
 api_hash = "138b992a0e672e8346d8439c3f42ea78"
@@ -10,6 +10,8 @@ app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 @app.on_message(filters.command("start"))
 def start(client, message):
+    message.chat.send_action("typing")
+    time.sleep(5)
     message.reply("Hello! Welcome to the bot!")
 
 # Handle the /help command
